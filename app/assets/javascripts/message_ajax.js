8255d37514de9f72e152f631a7a3e0592ee8f6e4
takahiro730 <takahironanasawa@gmail.com>
2019-09-27 15:17:50 +0900

    create_action

--- app/assets/javascripts/message_ajax.js
+++ app/assets/javascripts/message_ajax.js
@@ -1,4 +1,14 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<p>
+                  <strong>
+                    <a href=/users/${message.user_id}>${message.user_name}</a>
+                    :
+                  </strong>
+                  ${message.text}
+                </p>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
@@ -11,6 +21,11 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.textbox').val('')
     })
   })
 })
